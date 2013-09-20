229b3816bb0a3b9a007298afd069d5297f4663c6
James Gatannah <jamesgatannah@gmail.com>
2013-09-20 13:58:13 -0500

    Have something resembling the logging handlers in place as a unit test. Now I need to shift my vision of the architecture around to fit with that approach

--- test/protektor/log_test.clj
+++ test/protektor/log_test.clj
@@ -8,24 +8,19 @@
   "Pretend to pull back a bunch of logs from somewhere.
 Realistically, these should be lines of text. Although it's almost
 definitely to save logs to a database instead of just a txt file."
-  [[#inst "2013-09-20T12:37:00.000-00:00" [:normal "Entry One"]]
-   [#inst "2013-09-20T12:38:00.000-00:00" [:error "Fail"]]
-   [#inst "2013-09-20T12:38:30.000-00:00" [:warn "Something special should probably happen with this"]]
-   [#uuid "17c28986-2009-40f9-850f-ad5d389788c7" [:normal "This shouldn't work either"]]
-   [#inst "2013-09-20T12:54:00.000-00:00" [:normal "This shouldn't work"] "extra: causes error"]])
+  [[true :normal "Entry One"]
+   [true :error "Fail"]
+   [true :warn "Something special should probably happen with this"]
+   [false :normal "This shouldn't work either"]
+   [true :normal "This shouldn't work" "extra: causes error"]])
 
 (defn valid-entry? [entry]
-  (let [timestamp (edn/read-string (first entry))
-        remainder (rest entry)
-        severity (first remainder)
-        remainder (rest remainder)
-        text (first remainder)
-        remainder (rest remainder)]
-    (when (not (seq remainder))
-      (when (instance? java.sql.Timestamp timestamp)
-        )
-      
-)))
+  "This approach is stupid and more than a little pointless...but
+I don't have any actual interest in building a 'real' parser."
+  (if (= 3 (count entry))
+    (when (boolean time)
+      true)
+    (p/error :gibberish)))
 
 (defn raw->structured [[timestamp [severity text]]]
   "This isn't realistic from all sorts of different perspectives."
@@ -34,9 +29,22 @@ definitely to save logs to a database instead of just a txt file."
     (format "%s: %s\n%s\n" timestamp severity text)))
 
 (defn parse-log-entry [entry]
+  "This is where the interesting pieces happen"
   (if (valid-entry? text)
     (raw->structured entry)
-    (p/restart-case [])))
+    (p/restart-case [error :malformed-log-entry-error :text text]
+                    (skip-log-entry [_] nil)
+                    (use-value [value] value)
+                    (reparse-entry [fixed-text]
+                                   (parse-log-entry fixed-text)))))
+
+(defn analyze-entry [entry]
+  ;; This should pretend to do something useful with whatever parse-log-entry generated.
+  (println entry))
+
+(defn analyze-log [log]
+  (doseq [entry (parse-log-entry log)]
+    (analyze-entry entry)))
 
 (defn skip-log-entry [c]
   "Just skip entries that are wrong"
@@ -46,8 +54,13 @@ definitely to save logs to a database instead of just a txt file."
 (defn log-analyzer []
   "Simulate analyzing a collection of log files"
   (p/handler-bind [state :entering]
-                  [[malformed-log-entry-error skip-log-entry]]
+                  [[:malformed-log-entry-error skip-log-entry]
+                   [:error-entry (fn [c] (p/use-value
+                                          {:time-stamp "!!!!!" :severity "ERROR" :text (text c)}))]
+                   [:gibberish (fn [c] (p/invoke-restart reparse-entry))]]
                   (doseq [log (find-all-logs)]
                     (analyze-log log))))
 
-(facts "Malformed log handler")
+;;; What on earth can I do here that might be useful?
+(facts "Malformed log handler"
+       (fact "Overall" (log-analyzer) => nil?))
